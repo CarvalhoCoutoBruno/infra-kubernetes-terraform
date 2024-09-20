781cc96e06bef55de5a4a35a8a59ca6654c30b26
CarvalhoCoutoBruno <bruno.carvalho@gmail.com>
2024-09-20 20:44:09 -0300

    Adicionando o secret vazio

--- _values.tf
+++ _values.tf
@@ -4,28 +4,7 @@ locals {
   subscription_id     = "dd1b3041-17d1-49c4-854c-696224d72fb5"
   tenant_id           = "39eafb87-49d3-46eb-82b1-867b38750ebe"
   sp_client_id        = "923add4d-e2f6-434d-a11d-0e763d7046d6"
-  
-
-  cosmosdb = {
-    account_name = "skina-cosmos"
-    kind         = "MongoDB" # Outras opções: GlobalDocumentDB (SQL) e Parse
-    capabilities = [
-      "DisableRateLimitingResponses",
-      "EnableAggregationPipeline",
-      "EnableServerless",
-      "mongoEnableDocLevelTTL",
-      "EnableMongo",
-      "MongoDBv3.4",
-      "EnableMongoRetryableWrites"
-    ]
-    # Definição dos bancos de dados e suas coleções
-    databases = [
-      {
-        db_name     = "skina-db"
-        collections = ["skina-data"]
-      }
-    ]
-  }
+  sp_client_secret    = ""
 
   aks = {
     name     = "skina-k8s"
