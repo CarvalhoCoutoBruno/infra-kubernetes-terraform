f6730aa45b329be43c2a24c99bfd6ae74d9d03dc
CarvalhoCoutoBruno <bruno.carvalho@gmail.com>
2024-09-20 20:39:20 -0300

    Update _values.tf

--- _values.tf
+++ _values.tf
@@ -4,7 +4,6 @@ locals {
   subscription_id     = "dd1b3041-17d1-49c4-854c-696224d72fb5"
   tenant_id           = "39eafb87-49d3-46eb-82b1-867b38750ebe"
   sp_client_id        = "923add4d-e2f6-434d-a11d-0e763d7046d6"
-  sp_client_secret    = ""
 
   cosmosdb = {
     account_name = "skina-cosmos"
